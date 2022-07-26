e1ace2fdbc2c17d819ca9d4f5b35aa1c028f2988
Pariskrit19 <moktan.pariskrit@webexpertsnepal.com>
2022-07-26 17:02:58 +0545

    added attendance component

---
+++ src/components/AttendanceDetail/index.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function AttendanceDetail({ theme = "" }: { theme: string }) {
+	return (
+		<div
+			className="gx-attendance"
+			
+		>
+			<div>
+				<p>CHECK IN</p>
+				<p>20:30:30</p>
+			</div>
+			<div>
+				<p>CHECK OUT</p>
+				<p>20:30:30</p>
+			</div>
+			<div>
+				<p>TOTAL TIME</p>
+				<p>20:30:30</p>
+			</div>
+		</div>
+	);
+}
+
+export default AttendanceDetail;
--- src/containers/Topbar/index.js
+++ src/containers/Topbar/index.js
@@ -12,6 +12,7 @@ import Auxiliary from "util/Auxiliary";
 
 import {NAV_STYLE_DRAWER, NAV_STYLE_FIXED, NAV_STYLE_MINI_SIDEBAR, TAB_SIZE} from "constants/ThemeSetting";
 import {connect} from "react-redux";
+import AttendanceDetail from "../../components/AttendanceDetail";
 
 const {Header} = Layout;
 
@@ -44,12 +45,14 @@ class Topbar extends Component {
               />
             </div> : null}
           <Link to="/" className="gx-d-block gx-d-lg-none gx-pointer">
-            <img alt="" src={require("assets/images/w-logo.png")}/></Link>
+
+              <img alt="" src={require("assets/images/w-logo.png")}/></Link>
 
           <SearchBox styleName="gx-d-none gx-d-lg-block gx-lt-icon-search-bar-lg"
                      placeholder="Search in app..."
                      onChange={this.updateSearchChatUser.bind(this)}
                      value={this.state.searchText}/>
+                     <AttendanceDetail/>
           <ul className="gx-header-notifications gx-ml-auto">
             <li className="gx-notify gx-notify-search gx-d-inline-block gx-d-lg-none">
               <Popover overlayClassName="gx-popover-horizantal" placement="bottomRight" content={
