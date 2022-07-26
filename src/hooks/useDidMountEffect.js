4688f6aab16e818b0c9fdb378aa9b50efa88d0e4
ashok22ganika <ganika.ashok@webexpertsnepal.com>
2022-07-26 13:24:24 +0545

    hooks context api helpers added

---
+++ src/hooks/useDidMountEffect.js
@@ -0,0 +1,12 @@
+import { useEffect, useRef } from "react";
+
+function useDidMountEffect(func, depen) {
+	const ref = useRef(false);
+	useEffect(() => {
+		if (ref.current) func();
+		else ref.current = true;
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, depen);
+}
+
+export default useDidMountEffect;
