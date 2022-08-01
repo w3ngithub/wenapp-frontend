0bdb194e261f3ce456bd46ec42820935aa5948d8
Pariskrit19 <moktan.pariskrit@webexpertsnepal.com>
2022-08-01 13:14:25 +0545

    user detail form modal completed

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -2,8 +2,10 @@ import { BASE_API_PATH } from "helpers/api";
 
 // Users
 const BASE_API_PATH_USERS = `${BASE_API_PATH}users`;
+const BASE_API_PATH_ROLES = `${BASE_API_PATH_USERS}/roles`;
 
 export const Apis = {
 	// Users
-	Users: `${BASE_API_PATH_USERS}`
+	Users: `${BASE_API_PATH_USERS}`,
+	Roles: `${BASE_API_PATH_ROLES}`
 };
