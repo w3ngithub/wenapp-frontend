b259dffa7aff66f63b358359bf3c9c1f73420eae
Pariskrit19 <moktan.pariskrit@webexpertsnepal.com>
2022-08-11 08:43:44 +0545

    lms table completed and cancel api integrated

--- src/services/leaves/index.js
+++ src/services/leaves/index.js
@@ -11,4 +11,36 @@ const getLeaveDaysOfAllUsers = async () => {
 	}
 };
 
-export { getLeaveDaysOfAllUsers };
+const getLeavesOfAllUsers = async () => {
+	try {
+		let response = await API.get(`${Apis.Leaves}`);
+		return getAPIResponse(response);
+	} catch (err) {
+		return getAPIResponse(err?.response);
+	}
+};
+
+const getLeavesOfUser = async id => {
+	try {
+		let response = await API.get(`${Apis.Leaves}?user=${id}`);
+		return getAPIResponse(response);
+	} catch (err) {
+		return getAPIResponse(err?.response);
+	}
+};
+
+const changeLeaveStatus = async (id, statusType) => {
+	try {
+		let response = await API.patch(`${Apis.Leaves}/${id}/status/${statusType}`);
+		return getAPIResponse(response);
+	} catch (err) {
+		return getAPIResponse(err?.response);
+	}
+};
+
+export {
+	getLeaveDaysOfAllUsers,
+	getLeavesOfUser,
+	changeLeaveStatus,
+	getLeavesOfAllUsers
+};
