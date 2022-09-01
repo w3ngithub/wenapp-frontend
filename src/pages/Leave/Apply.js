762bdc063fe9485cb934baf0583d215126155109
Pariskrit19 <moktan.pariskrit@webexpertsnepal.com>
2022-09-01 14:27:44 +0545

    lms calendar holiday disable

--- src/pages/Leave/Apply.js
+++ src/pages/Leave/Apply.js
@@ -10,6 +10,7 @@ import { getTeamLeads } from "services/users/userDetails";
 import { notification } from "helpers/notification";
 import { THEME_TYPE_DARK } from "constants/ThemeSetting";
 import "react-multi-date-picker/styles/backgrounds/bg-dark.css";
+import { getAllHolidays } from "services/resources";
 
 const FormItem = Form.Item;
 const { TextArea } = Input;
@@ -22,6 +23,11 @@ function Apply({ ...rest }) {
 	const darkCalendar = themeType === THEME_TYPE_DARK;
 
 	const [leaveType, setLeaveType] = useState("");
+
+	const { data: Holidays } = useQuery(["DashBoardHolidays"], () =>
+		getAllHolidays({ sort: "-createdAt", limit: "1" })
+	);
+
 	const leaveTypeQuery = useQuery(["leaveType"], getLeaveTypes, {
 		select: res => [
 			...res?.data?.data?.data?.map(type => ({
@@ -75,137 +81,143 @@ function Apply({ ...rest }) {
 			});
 		});
 	};
+	const holidaysThisYear = Holidays?.data?.data?.data?.[0]?.holidays?.map(
+		holiday => ({
+			date: new Date(holiday?.date).getDate(),
+			name: holiday?.title
+		})
+	);
 
 	return (
-			<Spin spinning={leaveMutation.isLoading}>
-				<Form layout="vertical" style={{ padding: "15px 18px" }}>
-					<Row type="flex">
-						<Col
-							xs={24}
-							sm={6}
-							md={6}
-							style={{ flex: 0.3, marginRight: "6rem" }}
-						>
-							<FormItem label="Select Leave Dates">
-								{getFieldDecorator("leaveDates", {
-									rules: [{ required: true, message: "Required!" }]
-								})(
-									<Calendar
-										className={darkCalendar ? "bg-dark" : "null"}
-										numberOfMonths={1}
-										disableMonthPicker
-										disableYearPicker
-										multiple
-										minDate={
-											leaveType === "Sick"
-												? new DateObject().subtract(2, "months")
-												: new Date()
-										}
-										mapDays={({ date, today }) => {
-											let isWeekend = [0, 6].includes(date.weekDay.index);
-											if (isWeekend)
-												return {
-													disabled: true,
-													style: { color: "#ccc" },
-													onClick: () =>
-														alert(
-															isWeekend
-																? "weekends are disabled"
-																: "past dates are disabled"
-														)
-												};
-										}}
-									/>
-								)}
-							</FormItem>
-							<small style={{ color: "red", fontSize: "14px", width: "10%" }}>
-								*Disabled dates are holidays
-							</small>
-						</Col>
-						<Col span={18} xs={24} sm={24} md={15}>
-							<Row type="flex">
-								<Col span={12} xs={24} lg={12} md={24}>
-									<FormItem label="Leave Type">
-										{getFieldDecorator("leaveType", {
-											rules: [{ required: true, message: "Required!" }]
-										})(
-											<Select
-												showSearch
-												filterOption={filterOptions}
-												placeholder="Select Type"
-												style={{ width: "100%" }}
-												onChange={handleTypesChange}
-											>
-												{leaveTypeQuery?.data?.map(type => (
-													<Option value={type.id} key={type.id}>
-														{type.value}
-													</Option>
+		<Spin spinning={leaveMutation.isLoading}>
+			<Form layout="vertical" style={{ padding: "15px 18px" }}>
+				<Row type="flex">
+					<Col xs={24} sm={6} md={6} style={{ flex: 0.3, marginRight: "6rem" }}>
+						<FormItem label="Select Leave Dates">
+							{getFieldDecorator("leaveDates", {
+								rules: [{ required: true, message: "Required!" }]
+							})(
+								<Calendar
+									className={darkCalendar ? "bg-dark" : "null"}
+									numberOfMonths={1}
+									disableMonthPicker
+									disableYearPicker
+									weekStartDayIndex={1}
+									multiple
+									minDate={
+										leaveType === "Sick"
+											? new DateObject().subtract(2, "months")
+											: new Date()
+									}
+									mapDays={({ date, today }) => {
+										let isWeekend = [0, 6].includes(date.weekDay.index);
+										let holidayList = holidaysThisYear.filter(
+											holiday => date.day === holiday?.date
+										);
+										let isHoliday = holidayList?.length > 0;
+										if (isWeekend || isHoliday)
+											return {
+												disabled: true,
+												style: { color: isWeekend ? "#ccc" : "rgb(237 45 45)" },
+												onClick: () =>
+													alert(
+														isWeekend
+															? "weekends are disabled"
+															: `${holidayList[0]?.name} holiday`
+													)
+											};
+									}}
+								/>
+							)}
+						</FormItem>
+						<small style={{ color: "red", fontSize: "14px", width: "10%" }}>
+							*Disabled dates are holidays
+						</small>
+					</Col>
+					<Col span={18} xs={24} sm={24} md={15}>
+						<Row type="flex">
+							<Col span={12} xs={24} lg={12} md={24}>
+								<FormItem label="Leave Type">
+									{getFieldDecorator("leaveType", {
+										rules: [{ required: true, message: "Required!" }]
+									})(
+										<Select
+											showSearch
+											filterOption={filterOptions}
+											placeholder="Select Type"
+											style={{ width: "100%" }}
+											onChange={handleTypesChange}
+										>
+											{leaveTypeQuery?.data?.map(type => (
+												<Option value={type.id} key={type.id}>
+													{type.value}
+												</Option>
+											))}
+										</Select>
+									)}
+								</FormItem>
+							</Col>
+							<Col span={10} xs={24} lg={12} md={24}>
+								<FormItem label="Select Team Leads">
+									{getFieldDecorator("assignTo", {
+										rules: [{ required: true, message: "Required!" }]
+									})(
+										<Checkbox.Group style={{ width: "100%" }}>
+											<Row style={{ flexDirection: "row" }}>
+												{teamLeadsQuery?.data?.data?.map(lead => (
+													<Col span={12} key={lead._id}>
+														<Checkbox className="gx-mb-3" value={lead._id}>
+															{lead.name}
+														</Checkbox>
+													</Col>
 												))}
-											</Select>
-										)}
-									</FormItem>
-								</Col>
-								<Col span={10} xs={24} lg={12} md={24}>
-									<FormItem label="Select Team Leads">
-										{getFieldDecorator("assignTo", {
-											rules: [{ required: true, message: "Required!" }]
-										})(
-											<Checkbox.Group style={{ width: "100%" }}>
-												<Row style={{ flexDirection: "row" }}>
-													{teamLeadsQuery?.data?.data?.map(lead => (
-														<Col span={12} key={lead._id}>
-															<Checkbox className="gx-mb-3" value={lead._id}>
-																{lead.name}
-															</Checkbox>
-														</Col>
-													))}
-												</Row>
-											</Checkbox.Group>
-										)}
-									</FormItem>
-								</Col>
-							</Row>
-							<Row>
-								<Col span={24}>
-									<FormItem label="Leave Reason">
-										{getFieldDecorator("reason", {
-											rules: [
-												{
-													validator: (rule, value, callback) => {
-														try {
-															if (!value) throw new Error("Required!");
+											</Row>
+										</Checkbox.Group>
+									)}
+								</FormItem>
+							</Col>
+						</Row>
+						<Row>
+							<Col span={24}>
+								<FormItem label="Leave Reason">
+									{getFieldDecorator("reason", {
+										rules: [
+											{
+												validator: (rule, value, callback) => {
+													try {
+														if (!value) throw new Error("Required!");
 
-															const trimmedValue = value && value.trim();
-															if (trimmedValue?.length < 10) {
-																throw new Error(
-																	"Reason should be at least 10 letters!"
-																);
-															}
-														} catch (err) {
-															callback(err.message);
-															return;
+														const trimmedValue = value && value.trim();
+														if (trimmedValue?.length < 10) {
+															throw new Error(
+																"Reason should be at least 10 letters!"
+															);
 														}
-
-														callback();
+													} catch (err) {
+														callback(err.message);
+														return;
 													}
+
+													callback();
 												}
-											]
-										})(<TextArea placeholder="Enter Leave Reason" rows={10} />)}
-									</FormItem>
-									<div>
-										<Button type="primary" onClick={handleSubmit}>
-											Apply
-										</Button>
-										<Button type="danger" onClick={handleFormReset}>
-											Reset
-										</Button>
-									</div>
-								</Col>
-							</Row>
-						</Col>
-					</Row>
-				</Form>
-			</Spin>
+											}
+										]
+									})(<TextArea placeholder="Enter Leave Reason" rows={10} />)}
+								</FormItem>
+								<div>
+									<Button type="primary" onClick={handleSubmit}>
+										Apply
+									</Button>
+									<Button type="danger" onClick={handleFormReset}>
+										Reset
+									</Button>
+								</div>
+							</Col>
+						</Row>
+					</Col>
+				</Row>
+			</Form>
+		</Spin>
 	);
 }
 
