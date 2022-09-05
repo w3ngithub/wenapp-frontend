fd947a2a84321e626fa5ae4295d18120e5f41b5d
RaulShahi1 <shahi.raul@webexpertsnepal.com>
2022-09-05 12:37:38 +0545

    sidebar scrolling fixed

--- src/util/CustomScrollbars.js
+++ src/util/CustomScrollbars.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Scrollbars } from "react-custom-scrollbars";
 
-const CustomScrollbars = props => (
-	<Scrollbars
-		{...props}
-		autoHide
-		renderTrackHorizontal={props => (
-			<div
-				{...props}
-				style={{ display: "none" }}
-				className="track-horizontal"
-			/>
-		)}
-	/>
-);
+const CustomScrollbars = props => {
+	const getWindowsSize = () => {
+		const { innerHeight } = window;
+		return innerHeight;
+	};
+	const [windowSize, setWindowSize] = useState(getWindowsSize());
+
+	useEffect(() => {
+		const handleWindowResize = () => {
+			setWindowSize(getWindowsSize());
+		};
+
+		window.addEventListener("resize", handleWindowResize);
+
+		return () => {
+			window.removeEventListener("resize", handleWindowResize);
+		};
+	}, []);
+
+	return (
+		<Scrollbars
+			{...props}
+			autoHide
+			autoHeight
+			autoHeightMin={windowSize - 100}
+			renderTrackHorizontal={props => (
+				<div
+					{...props}
+					style={{ display: "none" }}
+					className="track-horizontal"
+				/>
+			)}
+		/>
+	);
+};
 export default CustomScrollbars;
