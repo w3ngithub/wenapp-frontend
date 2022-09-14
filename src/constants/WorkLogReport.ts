ce94e7c8cff5828254fa5daee61b7040e3a62ad9
ashok22ganika <ganika.ashok@webexpertsnepal.com>
2022-09-14 17:17:14 +0545

    work log report api addition

--- src/constants/WorkLogReport.ts
+++ src/constants/WorkLogReport.ts
@@ -1,42 +1,42 @@
-import { ReactElement } from "react";
+import {ReactElement} from 'react'
 
 interface report {
-	title: string;
-	dataIndex?: string;
-	key: any;
-	width?: number;
-	sorter?: (a: any, b: any) => any;
-	sortOrder?: any;
-	render?: (text: any, record: any) => ReactElement | null;
+  title: string
+  dataIndex?: string
+  key: any
+  width?: number
+  sorter?: (a: any, b: any) => any
+  sortOrder?: any
+  render?: (text: any, record: any) => ReactElement | null
 }
 
 const WORK_LOG_REPORT_COLUMNS = (sortedInfo: any): report[] => [
-	{
-		title: "Users",
-		dataIndex: "user",
-		key: "user",
-		sorter: (a, b) => {
-			return a.user.toString().localeCompare(b.user.toString());
-		},
-		sortOrder: sortedInfo.columnKey === "user" && sortedInfo.order
-	},
-	{
-		title: "Details",
-		dataIndex: "details",
-		key: "details",
-		sorter: (a, b) =>
-			a.details?.toString().localeCompare(b.details?.toString()),
-		sortOrder: sortedInfo.columnKey === "details" && sortedInfo.order
-	},
-	{
-		title: "Time Spent",
-		dataIndex: "timeSpent",
-		key: "timeSpent",
-		sorter: (a, b) =>
-			a.timeSpent?.toString().localeCompare(b.timeSpent?.toString()),
+  {
+    title: 'Users',
+    dataIndex: 'user',
+    key: 'user',
+    sorter: (a, b) => {
+      return a.user.toString().localeCompare(b.user.toString())
+    },
+    sortOrder: sortedInfo.columnKey === 'user' && sortedInfo.order,
+  },
+  {
+    title: 'Details',
+    dataIndex: 'details',
+    key: 'details',
+    sorter: (a, b) =>
+      a.details?.toString().localeCompare(b.details?.toString()),
+    sortOrder: sortedInfo.columnKey === 'details' && sortedInfo.order,
+  },
+  {
+    title: 'Total Time Spent',
+    dataIndex: 'timeSpent',
+    key: 'timeSpent',
+    sorter: (a, b) =>
+      a.timeSpent?.toString().localeCompare(b.timeSpent?.toString()),
 
-		sortOrder: sortedInfo.columnKey === "timeSpent" && sortedInfo.order
-	}
-];
+    sortOrder: sortedInfo.columnKey === 'timeSpent' && sortedInfo.order,
+  },
+]
 
-export { WORK_LOG_REPORT_COLUMNS };
+export {WORK_LOG_REPORT_COLUMNS}
--- src/pages/Reports/WorkLogReport/index.tsx
+++ src/pages/Reports/WorkLogReport/index.tsx
@@ -1,145 +1,182 @@
-import React, { useState } from "react";
-import { Button, Card, Form, Table } from "antd";
-import RangePicker from "components/Elements/RangePicker";
-import { intialDate } from "constants/Attendance";
-import Select from "components/Elements/Select";
-import { useQuery } from "@tanstack/react-query";
-import { getLogTypes } from "services/timeLogs";
-import { getAllProjects } from "services/projects";
-import { getAllUsers } from "services/users/userDetails";
-import { WORK_LOG_REPORT_COLUMNS } from "constants/WorkLogReport";
+import React, {useState} from 'react'
+import {Button, Card, Form, Table} from 'antd'
+import RangePicker from 'components/Elements/RangePicker'
+import {intialDate} from 'constants/Attendance'
+import Select from 'components/Elements/Select'
+import {useQuery} from '@tanstack/react-query'
+import {getLogTypes, getWorkLogReport} from 'services/timeLogs'
+import {getAllProjects} from 'services/projects'
+import {getAllUsers} from 'services/users/userDetails'
+import {WORK_LOG_REPORT_COLUMNS} from 'constants/WorkLogReport'
+import moment from 'moment'
+import {changeDate, toRoundoff} from 'helpers/utils'
 
-const FormItem = Form.Item;
+const FormItem = Form.Item
 
-function WorkLogReport() {
-	//init state
-	const [sort, setSort] = useState({});
-	const [form] = Form.useForm()
-	const [date, setDate] = useState(intialDate);
-	const [logType, setLogType] = useState<string | undefined>(undefined);
-	const [project, setProject] = useState<string | undefined>(undefined);
-	const [user, setUser] = useState<string | undefined>(undefined);
-
-	//init hooks
-	const { data: logTypesData } = useQuery(["logTypes"], getLogTypes);
-
-	const { data: projectData } = useQuery(["WorkLogProjects"], () =>
-		getAllProjects({
-			fields:
-				"_id,name,-devOps,-createdBy,-designers,-developers,-projectStatus,-projectTags,-projectTypes,-qa,-updatedBy"
-		})
-	);
-
-	const { data: usersData } = useQuery(["WorkLogusers"], () =>
-		getAllUsers({
-			active: "true",
-			fields: "_id,name"
-		})
-	);
-
-	const handleChangeDate = (date: any[]) => {
-		setDate(date);
-	};
-
-	const handleLogTypeChange = (typeId: string) => {
-		setLogType(typeId);
-	};
-
-	const handleProjectChange = (ProjectId: string) => {
-		setProject(ProjectId);
-	};
-
-	const handleUserChange = (userId: string) => {
-		setUser(userId);
-	};
-
-	const handleTableChange = (pagination: any, filters: any, sorter: any) => {
-		setSort(sorter);
-	};
-
-	const handleResetFilter = () => {
-		setDate(intialDate);
-		setLogType(undefined);
-		setProject(undefined);
-		setUser(undefined);
-	};
-
-	return (
-		<Card title="Work Log Report">
-			<div className="components-table-demo-control-bar">
-				<div className="gx-d-flex gx-justify-content-between gx-flex-row">
-					<Form layout="inline" form={form}>
-						<FormItem>
-							<RangePicker handleChangeDate={handleChangeDate} date={date} />
-						</FormItem>
-						<FormItem className="direct-form-item">
-							<Select
-								placeholder="Select Project"
-								onChange={handleProjectChange}
-								value={project}
-								options={projectData?.data?.data?.data?.map((x: any) => ({
-									...x,
-									id: x._id,
-									value: x.name
-								}))}
-								inputSelect
-							/>
-						</FormItem>
-						<FormItem className="direct-form-item">
-							<Select
-								placeholder="Select Co-worker"
-								onChange={handleUserChange}
-								value={user}
-								options={usersData?.data?.data?.data?.map((x: any) => ({
-									...x,
-									id: x._id,
-									value: x.name
-								}))}
-							/>
-						</FormItem>
-						<FormItem className="direct-form-item">
-							<Select
-								placeholder="Select Log Type"
-								onChange={handleLogTypeChange}
-								value={logType}
-								options={logTypesData?.data?.data?.data?.map((x: any) => ({
-									...x,
-									id: x._id,
-									value: x.name
-								}))}
-							/>
-						</FormItem>
-
-						<FormItem>
-							<Button
-								className="gx-btn gx-btn-primary gx-text-white gx-mt-auto"
-								onClick={handleResetFilter}
-							>
-								Reset
-							</Button>
-						</FormItem>
-					</Form>
-				</div>
-			</div>
-			<Table
-				className="gx-table-responsive"
-				columns={WORK_LOG_REPORT_COLUMNS(sort)}
-				// dataSource={formattedWeeklyReports(data?.data?.data?.report, clients)}
-				onChange={handleTableChange}
-				// pagination={{
-				// 	current: page.page,
-				// 	pageSize: page.limit,
-				// 	pageSizeOptions: ["5", "10", "20", "50"],
-				// 	showSizeChanger: true,
-				// 	total: data?.data?.data?.count || 1,
-				// 	onShowSizeChange,
-				// 	hideOnSinglePage: true,
-				// 	onChange: handlePageChange
-				// }}
-				// loading={isLoading || isFetching}
-			/>
-		</Card>
-	);
+const formattedWorkLogReport: any = (logs: any) => {
+  return logs?.map((log: any) => ({
+    ...log,
+    user: log?._id?.[0]?.name,
+    timeSpent: toRoundoff(log?.totalTimeSpent),
+    details: Object.values(log?.timeLogs)?.map((x: any, i: number) => (
+      <div style={{marginBottom: 10}} key={i}>
+        <span>{changeDate(x?.[0]?.logDate)}</span>
+        {x.map((item: any) => (
+          <div className=" gx-d-flex" key={item.remarks + item.totalHours}>
+            <span>{item.project?.[0]?.name || 'Other'}</span>
+            <span className="gx-ml-5">
+              -{item.remarks} ({toRoundoff(item.totalHours)}hrs)
+            </span>
+          </div>
+        ))}
+      </div>
+    )),
+  }))
 }
 
-export default WorkLogReport;
+function WorkLogReport() {
+  //init state
+  const [sort, setSort] = useState({})
+  const [form] = Form.useForm()
+  const [date, setDate] = useState(intialDate)
+  const [logType, setLogType] = useState<string | undefined>(undefined)
+  const [project, setProject] = useState<string | undefined>(undefined)
+  const [user, setUser] = useState<string | undefined>(undefined)
+
+  //init hooks
+
+  const {data, isFetching} = useQuery(
+    ['workLogReports', user, project, logType, date],
+    () =>
+      getWorkLogReport({
+        user,
+        project,
+        logType,
+        fromDate: date?.[0] ? moment.utc(date[0]).format() : '',
+        toDate: date?.[1] ? moment.utc(date[1]).format() : '',
+      })
+  )
+
+  const {data: logTypesData} = useQuery(['logTypes'], getLogTypes)
+
+  const {data: projectData} = useQuery(['WorkLogProjects'], () =>
+    getAllProjects({
+      fields:
+        '_id,name,-devOps,-createdBy,-designers,-developers,-projectStatus,-projectTags,-projectTypes,-qa,-updatedBy',
+    })
+  )
+
+  const {data: usersData} = useQuery(['WorkLogusers'], () =>
+    getAllUsers({
+      active: 'true',
+      fields: '_id,name',
+    })
+  )
+
+  const handleChangeDate = (date: any[]) => {
+    setDate(date)
+  }
+
+  const handleLogTypeChange = (typeId: string) => {
+    setLogType(typeId)
+  }
+
+  const handleProjectChange = (ProjectId: string) => {
+    setProject(ProjectId)
+  }
+
+  const handleUserChange = (userId: string) => {
+    setUser(userId)
+  }
+
+  const handleTableChange = (pagination: any, filters: any, sorter: any) => {
+    setSort(sorter)
+  }
+
+  const handleResetFilter = () => {
+    setDate(intialDate)
+    setLogType(undefined)
+    setProject(undefined)
+    setUser(undefined)
+  }
+
+  const logData: any[] =
+    data?.data?.data?.report.map((x: any) => ({
+      ...x,
+      timeLogs: x?.timeLogs?.reduce((acc: any, item: any) => {
+        const logDate = item.logDate.split('T')[0]
+        acc[logDate] = (acc[logDate] || []).concat(item)
+        return acc
+      }, {}),
+    })) || []
+
+  return (
+    <Card title="Work Log Report">
+      <div className="components-table-demo-control-bar">
+        <div className="gx-d-flex gx-justify-content-between gx-flex-row">
+          <Form layout="inline" form={form}>
+            <FormItem>
+              <RangePicker handleChangeDate={handleChangeDate} date={date} />
+            </FormItem>
+            <FormItem className="direct-form-item">
+              <Select
+                placeholder="Select Project"
+                onChange={handleProjectChange}
+                value={project}
+                options={projectData?.data?.data?.data?.map((x: any) => ({
+                  ...x,
+                  id: x._id,
+                  value: x.name,
+                }))}
+                inputSelect
+              />
+            </FormItem>
+            <FormItem className="direct-form-item">
+              <Select
+                placeholder="Select Co-worker"
+                onChange={handleUserChange}
+                value={user}
+                options={usersData?.data?.data?.data?.map((x: any) => ({
+                  ...x,
+                  id: x._id,
+                  value: x.name,
+                }))}
+              />
+            </FormItem>
+            <FormItem className="direct-form-item">
+              <Select
+                placeholder="Select Log Type"
+                onChange={handleLogTypeChange}
+                value={logType}
+                options={logTypesData?.data?.data?.data?.map((x: any) => ({
+                  ...x,
+                  id: x._id,
+                  value: x.name,
+                }))}
+              />
+            </FormItem>
+
+            <FormItem>
+              <Button
+                className="gx-btn gx-btn-primary gx-text-white gx-mt-auto"
+                onClick={handleResetFilter}
+              >
+                Reset
+              </Button>
+            </FormItem>
+          </Form>
+        </div>
+      </div>
+      <Table
+        className="gx-table-responsive"
+        columns={WORK_LOG_REPORT_COLUMNS(sort)}
+        dataSource={formattedWorkLogReport(logData)}
+        onChange={handleTableChange}
+        pagination={false}
+        loading={isFetching}
+      />
+    </Card>
+  )
+}
+
+export default WorkLogReport
