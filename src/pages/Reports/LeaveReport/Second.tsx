ca5969a66d0183263ff42a999b325ef1aa77325d
ashok22ganika <ganika.ashok@webexpertsnepal.com>
2022-09-21 14:36:50 +0545

    leave report quarter added

---
+++ src/pages/Reports/LeaveReport/Second.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import CommonQuarters from './Common'
+
+function Second({fromDate, toDate}: {fromDate: any; toDate: any}) {
+  return <CommonQuarters fromDate={fromDate} toDate={toDate} />
+}
+
+export default Second
