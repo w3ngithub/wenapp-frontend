fae807363992817673396c4a0985e4fc7ea237d6
ashok22ganika <ganika.ashok@webexpertsnepal.com>
2022-09-27 16:19:23 +0545

    leave report completed

--- src/pages/Reports/LeaveReport/First.tsx
+++ src/pages/Reports/LeaveReport/First.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import CommonQuarters from './Common'
 
-function First({fromDate, toDate}: {fromDate: any; toDate: any}) {
-  return <CommonQuarters fromDate={fromDate} toDate={toDate} />
+function First({
+  fromDate,
+  toDate,
+  quarter,
+  positions,
+}: {
+  fromDate: any
+  toDate: any
+  quarter: number
+  positions: any
+}) {
+  return (
+    <CommonQuarters
+      fromDate={fromDate}
+      toDate={toDate}
+      quarter={quarter}
+      positions={positions}
+    />
+  )
 }
 
 export default First
