b490e3bbaa8c2460e540b9a691f06f7cba925f95
Pariskrit19 <moktan.pariskrit@webexpertsnepal.com>
2022-09-28 12:11:25 +0545

    fixed design issues

--- src/pages/Leave/LeavesCalendar.tsx
+++ src/pages/Leave/LeavesCalendar.tsx
@@ -1,46 +1,62 @@
-import { Card } from "antd";
-import React from "react";
-import { useQuery } from "@tanstack/react-query";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
-import { getFiscalYearLeaves } from "services/leaves";
+import {Card} from 'antd'
+import React from 'react'
+import {useQuery} from '@tanstack/react-query'
+import {Calendar, momentLocalizer} from 'react-big-calendar'
+import moment from 'moment'
+import {getFiscalYearLeaves} from 'services/leaves'
 
-const localizer = momentLocalizer(moment);
+const localizer = momentLocalizer(moment)
 
 const LeavesCalendar = () => {
-	const leavesQuery = useQuery(
-		["leavesCalendar"],
-		() => getFiscalYearLeaves(),
-		{
-			onError: err => console.log(err)
-		}
-	);
-		
-	const leaveUsers = leavesQuery?.data?.data?.data?.data?.map(({ _id }: any) => {
-		const nameSplitted= _id?.user[0].split(" ");
-		const lastName = `${nameSplitted.pop().substring(0,1)}.`;
-		const shortName = `${nameSplitted.join(" ")} ${lastName}`
+  const leavesQuery = useQuery(
+    ['leavesCalendar'],
+    () => getFiscalYearLeaves(),
+    {
+      onError: err => console.log(err),
+    }
+  )
 
-		return {
-			title: shortName,
-			start: new Date(_id.leaveDates),
-			end: new Date(_id.leaveDates)
-		}
-	})
+  const leaveUsers = leavesQuery?.data?.data?.data?.data?.map(({_id}: any) => {
+    const nameSplitted = _id?.user[0].split(' ')
+    const lastName = `${nameSplitted.pop().substring(0, 1)}.`
+    const shortName = `${nameSplitted.join(' ')} ${lastName}`
 
-	return (
-		<Card className="gx-card" title="Calendar">
-			<div className="gx-rbc-calendar">
-				<Calendar
-					localizer={localizer}
-					events={leaveUsers}
-					startAccessor="start"
-					endAccessor="end"
-					popup
-				/>
-			</div>
-		</Card>
-	);
-};
+    return {
+      title: shortName,
+      start: new Date(_id.leaveDates),
+      end: new Date(_id.leaveDates),
+    }
+  })
 
-export default LeavesCalendar;
+  const handleEventStyle = (event: any) => {
+    let style: any = {
+      color: 'white',
+      padding: '1px 10px',
+      width: 'fit-content',
+      margin: 'auto',
+      marginBottom: '0.2rem',
+      height: 'auto',
+    }
+
+    return {
+      style,
+    }
+  }
+
+  return (
+    <Card className="gx-card" title="Calendar">
+      <div className="gx-rbc-calendar">
+        <Calendar
+          eventPropGetter={handleEventStyle}
+          localizer={localizer}
+          events={leaveUsers}
+          startAccessor="start"
+          endAccessor="end"
+          popup
+        />
+      </div>
+    </Card>
+  )
+}
+
+export default LeavesCalendar
