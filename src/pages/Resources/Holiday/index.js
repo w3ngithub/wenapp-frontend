63bf2870029d403ee00ede004bc65cc3e0fcda79
dangolSusan <dangol.susan@webexpertsnepal.com>
2022-11-18 11:51:19 +0545

    added created date

--- src/pages/Resources/Holiday/index.js
+++ src/pages/Resources/Holiday/index.js
@@ -48,7 +48,9 @@ function Holiday() {
     data: Holidays,
     isLoading,
     isFetching,
-  } = useQuery(['DashBoardHolidays'], () => getAllHolidays({limit: '1'}))
+  } = useQuery(['DashBoardHolidays'], () =>
+    getAllHolidays({sort: '-createdAt', limit: '1'})
+  )
 
   Holidays?.data?.data?.data?.[0]?.holidays?.sort(compare)
 
