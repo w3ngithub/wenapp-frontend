29a4dc82a8b629c2e62e3a67b6334df611956675
RaulShahi1 <shahi.raul@webexpertsnepal.com>
2022-12-09 15:05:51 +0545

    divider color removed from announcements and holidays

--- src/components/Modules/dashboard/CRM/ActivityItem.js
+++ src/components/Modules/dashboard/CRM/ActivityItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Avatar, Divider} from 'antd'
 import Auxiliary from 'util/Auxiliary'
 
-const ActivityItem = ({task}) => {
+const ActivityItem = ({task, title = ''}) => {
   return (
     <Auxiliary>
       <div>{task?.title}</div>
@@ -30,7 +30,15 @@ const ActivityItem = ({task}) => {
           })}
         </ul>
       ) : null}
-      <Divider />
+      <p
+        className={
+          title === 'Notifications' || title === 'Recent Activities'
+            ? 'notificationDivider'
+            : ''
+        }
+      >
+        <Divider />
+      </p>
     </Auxiliary>
   )
 }
