c8125b8e1ea6a5bb70e5f02af7703dafb7f91716
Pariskrit19 <moktan.pariskrit@webexpertsnepal.com>
2022-12-13 16:47:41 +0545

    working on maintenance

--- src/components/Modules/Maintainance/index.js
+++ src/components/Modules/Maintainance/index.js
@@ -1,25 +1,47 @@
-import {isAdmin} from '@firebase/util'
 import {Switch} from 'antd'
 import React, {useState} from 'react'
 import {useSelector} from 'react-redux'
 import {selectAuthUser} from 'appRedux/reducers/Auth'
 import {useLocation} from 'react-router-dom'
+import {useMutation} from '@tanstack/react-query'
+import {updateMaintenance} from 'services/configurations'
+import {handleResponse} from 'helpers/utils'
+import {notification} from 'helpers/notification'
 
 const MaintainanceBar = () => {
   const userDetail = useSelector(selectAuthUser)
   const [isOn, setIsOn] = useState(false)
   const location = useLocation()
-
-  const handleMaintainance = async (e) => {
-    if (e) {
-      localStorage.setItem('isAdmin', isAdmin)
-      setIsOn(e)
-    } else {
-      setIsOn(e)
+  const maintenanceMutation = useMutation(
+    (payload) => updateMaintenance(payload),
+    {
+      onSuccess: (response) => {
+        const res = response?.data?.data
+        handleResponse(
+          response,
+          `Maintenance mode ${res?.isMaintenanceEnabled ? 'on' : 'off'}`,
+          'Maintenance mode change failed',
+          []
+        )
+        if (res?.isMaintenanceEnabled) {
+          localStorage.setItem('isAdmin', true)
+        } else {
+          localStorage.removeItem('isAdmin')
+        }
+      },
+      onError: (error) => {
+        notification({message: 'Maintenance mode change failed', type: 'error'})
+      },
     }
+  )
+
+  const handleMaintainance = (isActive) => {
+    maintenanceMutation.mutate({isMaintenanceEnabled: isActive})
+    setIsOn(isActive)
   }
 
-  return userDetail?.role?.key === 'admin' ? (
+  return userDetail?.role?.key === 'admin' ||
+    localStorage.getItem('isAdmin') ? (
     <>
       <span
         style={{
@@ -35,6 +57,7 @@ const MaintainanceBar = () => {
         unCheckedChildren="OFF"
         defaultChecked={location.pathname.includes('maintenance')}
         onChange={handleMaintainance}
+        loading={maintenanceMutation.isLoading}
       />
     </>
   ) : (
