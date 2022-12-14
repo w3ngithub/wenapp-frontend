3a9242f31196e772cf05b04ba56c7fde92c960cd
Pariskrit19 <moktan.pariskrit@webexpertsnepal.com>
2022-12-14 13:54:08 +0545

    created maintenance service

---
+++ src/services/configurations/index.ts
@@ -0,0 +1,12 @@
+import API from 'helpers/api'
+import {Apis} from 'services/api'
+import {getAPIResponse} from 'helpers/getApiResponse'
+
+export const updateMaintenance = async (payload: any) => {
+  try {
+    let response = await API.patch(`${Apis.Configurations}/update`, payload)
+    return getAPIResponse(response)
+  } catch (err) {
+    return getAPIResponse(err?.response)
+  }
+}
