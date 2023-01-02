290d4c38bad422912273bbdcb41971d5db8e72ed
RaulShahi1 <shahi.raul@webexpertsnepal.com>
2023-01-02 10:07:22 +0545

    x label hidden on mobile screen

--- src/routes/extensions/charts/recharts/bar/Components/TinyBarChart.js
+++ src/routes/extensions/charts/recharts/bar/Components/TinyBarChart.js
@@ -1,4 +1,5 @@
 import {THEME_TYPE_DARK} from 'constants/ThemeSetting'
+import useWindowsSize from 'hooks/useWindowsSize'
 import React from 'react'
 import {useSelector} from 'react-redux'
 import {
@@ -18,6 +19,7 @@ import {
 const TinyBarChart = ({data}) => {
   const {themeType} = useSelector((state) => state.settings)
   const darkTheme = themeType === THEME_TYPE_DARK
+  const {innerWidth} = useWindowsSize()
   return (
     <ResponsiveContainer width="100%" height={400}>
       <BarChart data={data} margin={{top: 10, right: 0, left: -15, bottom: 0}}>
@@ -25,6 +27,7 @@ const TinyBarChart = ({data}) => {
           dataKey="name"
           stroke={darkTheme ? '#fff' : '#000'}
           interval={0}
+          tick={innerWidth < 600 ? false : true}
         />
         <YAxis stroke={darkTheme ? '#fff' : '#000'} />
         <CartesianGrid strokeDasharray="3 3" />
