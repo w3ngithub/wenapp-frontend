98f258e38d42dd472aaaaa8380fe8856cb2e6357
RaulShahi1 <shahi.raul@webexpertsnepal.com>
2023-01-31 17:14:17 +0545

    task completed

--- src/pages/Resources/Holiday/CommonModal.tsx
+++ src/pages/Resources/Holiday/CommonModal.tsx
@@ -1,6 +1,7 @@
 import {MinusCircleOutlined, PlusOutlined} from '@ant-design/icons'
 import {
   Button,
+  Checkbox,
   Col,
   DatePicker,
   Form,
@@ -10,7 +11,6 @@ import {
   Spin,
   Table,
 } from 'antd'
-import {EDIT_HOLIDAY_COLUMNS} from 'constants/Holidays'
 import {notification} from 'helpers/notification'
 import useWindowsSize from 'hooks/useWindowsSize'
 import moment from 'moment'
@@ -50,35 +50,34 @@ const CommonModal = (props: modalType) => {
 
   const handleSubmit = () => {
     let values = form.getFieldsValue()
-    console.log('holidays values', values)
-    // let holidays = values?.holidays?.map(
-    //   (data: {date: any; title: string; remarks: string}, index: number) => {
-    //     if (data?.date || data?.title || data?.remarks) {
-    //       return true
-    //     } else return false
-    //   }
-    // )
-    // if (holidays.length === 0) {
-    //   return notification({type: 'info', message: 'Please Add Holiday Field'})
-    // }
-    // if (!holidays.includes(true)) {
-    //   holidays[0] = true
-    // }
-    // setIndexes(holidays)
-    // form
-    //   .validateFields()
-    //   .then((values) => {
-    //     onSubmit({
-    //       holidays: form
-    //         .getFieldsValue()
-    //         .holidays.filter((holiday: holidayType) => holiday)
-    //         .map((holiday: holidayType) => ({
-    //           ...holiday,
-    //           date: moment.utc(holiday.date).format(),
-    //         })),
-    //     })
-    //   })
-    //   .catch((err) => console.log(err))
+    let holidays = values?.holidays?.map(
+      (data: {date: any; title: string; remarks: string}, index: number) => {
+        if (data?.date || data?.title || data?.remarks) {
+          return true
+        } else return false
+      }
+    )
+    if (holidays.length === 0) {
+      return notification({type: 'info', message: 'Please Add Holiday Field'})
+    }
+    if (!holidays.includes(true)) {
+      holidays[0] = true
+    }
+    setIndexes(holidays)
+    form
+      .validateFields()
+      .then((values) => {
+        onSubmit({
+          holidays: form
+            .getFieldsValue()
+            .holidays.filter((holiday: holidayType) => holiday)
+            .map((holiday: holidayType) => ({
+              ...holiday,
+              date: moment.utc(holiday.date).format(),
+            })),
+        })
+      })
+      .catch((err) => console.log(err))
   }
 
   useEffect(() => {
@@ -93,21 +92,6 @@ const CommonModal = (props: modalType) => {
     }
     if (!toggle) form.resetFields()
   }, [toggle])
-
-  // useEffect(() => {
-  //   if (toggle) {
-  //     if (isEditMode)
-  //       editData.forEach((item: any, index: number) => {
-  //         form.setFieldsValue({
-  //           [`${item?._id}date`]: moment(item?.date),
-  //           [`${item?._id}title`]: item?.title,
-  //           [`${item?._id}remarks`]: item?.remarks,
-  //           [`${item?._id}allowLeaveApply`]: item?.allowLeaveApply,
-  //         })
-  //       })
-  //   }
-  //   if (!toggle) form.resetFields()
-  // }, [toggle])
 
   return (
     <Modal
@@ -127,6 +111,26 @@ const CommonModal = (props: modalType) => {
       ]}
     >
       <Spin spinning={isLoading}>
+        <Row
+          style={{
+            // columnGap: 20,
+            marginLeft: innerWidth <= 748 ? '' : '0.8rem',
+            marginBottom: '0.4rem',
+          }}
+        >
+          <Col span={6} lg={5} sm={5}>
+            <label>Date</label>
+          </Col>
+          <Col span={6} lg={5} sm={6}>
+            <label>Title</label>
+          </Col>
+          <Col span={6} lg={5} sm={6}>
+            <label>Remarks</label>
+          </Col>
+          <Col span={6} lg={6} sm={6}>
+            <label>Allow Leave Application</label>
+          </Col>
+        </Row>
         <Form
           form={form}
           name="dynamic_form_nest_item"
@@ -134,33 +138,6 @@ const CommonModal = (props: modalType) => {
           style={{marginLeft: 10}}
           className="add-holiday"
         >
-          {/* <Table
-            className="gx-table-responsive"
-            columns={EDIT_HOLIDAY_COLUMNS()}
-            dataSource={dataSource}
-            // onChange={handleTableChange}
-            pagination={false}
-            loading={isLoading}
-          /> */}
-          <Row
-            style={{
-              columnGap: 6,
-              marginLeft: innerWidth <= 748 ? '' : '1rem',
-            }}
-          >
-            <Col>
-              <label>Date</label>
-            </Col>
-            <Col>
-              <label>Title</label>
-            </Col>
-            <Col>
-              <label>Remarks</label>
-            </Col>
-            <Col>
-              <label>Allow Leave Application</label>
-            </Col>
-          </Row>
           <Form.List
             name="holidays"
             initialValue={[
@@ -182,11 +159,11 @@ const CommonModal = (props: modalType) => {
                   <Row
                     key={field.key}
                     style={{
-                      columnGap: 6,
+                      columnGap: 8,
                       marginLeft: innerWidth <= 748 ? '' : '1rem',
                     }}
                   >
-                    <Col span={24} sm={7}>
+                    <Col span={24} sm={7} lg={5}>
                       <Form.Item
                         noStyle
                         shouldUpdate={(prevValues, curValues) =>
@@ -196,7 +173,6 @@ const CommonModal = (props: modalType) => {
                       >
                         <Form.Item
                           {...field}
-                          // label="Date"
                           name={[field.name, 'date']}
                           required={false}
                           rules={[
@@ -210,7 +186,7 @@ const CommonModal = (props: modalType) => {
                         </Form.Item>
                       </Form.Item>
                     </Col>
-                    <Col span={24} sm={7}>
+                    <Col span={24} sm={7} lg={7}>
                       <Form.Item
                         noStyle
                         shouldUpdate={(prevValues, curValues) =>
@@ -220,7 +196,6 @@ const CommonModal = (props: modalType) => {
                       >
                         <Form.Item
                           {...field}
-                          // label="Title"
                           name={[field.name, 'title']}
                           required={false}
                           rules={[
@@ -238,30 +213,35 @@ const CommonModal = (props: modalType) => {
                         </Form.Item>
                       </Form.Item>
                     </Col>
-                    <Col span={24} sm={9}>
-                      <Row align="middle">
-                        <Col span={24} sm={20}>
-                          <Form.Item
-                            {...field}
-                            // label="Remarks"
-                            name={[field.name, 'remarks']}
-                            // rules={[{ required: true, message: "required!" }]}
-                          >
-                            <Input.TextArea rows={1} />
-                          </Form.Item>
-                        </Col>
-                        <Col span={24} sm={2}>
-                          <MinusCircleOutlined
-                            onClick={() => remove(field.name)}
-                            style={{marginBottom: 20}}
-                            className="svg-clear"
-                          />
-                        </Col>
-                      </Row>
+                    <Col span={24} sm={20} lg={7}>
+                      <Form.Item
+                        {...field}
+                        name={[field.name, 'remarks']}
+                        // rules={[{ required: true, message: "required!" }]}
+                      >
+                        <Input.TextArea rows={1} />
+                      </Form.Item>
+                    </Col>
+                    <Col span={24} sm={2}>
+                      <Form.Item
+                        {...field}
+                        name={[field.name, 'allowLeaveApply']}
+                        required={false}
+                        valuePropName="checked"
+                      >
+                        <Checkbox style={{marginLeft: '2rem'}} />
+                      </Form.Item>
+                    </Col>
+
+                    <Col span={6} sm={2} lg={2}>
+                      <MinusCircleOutlined
+                        onClick={() => remove(field.name)}
+                        style={{marginBottom: 20, marginTop: 10}}
+                        className="svg-clear"
+                      />
                     </Col>
                   </Row>
                 ))}
-
                 <Form.Item>
                   <Button
                     type="dashed"
