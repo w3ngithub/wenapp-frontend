dffc897fa83fedafe41253c7de26aee3bb0e8ed0
dangolSusan <dangol.susan@webexpertsnepal.com>
2023-01-31 13:39:57 +0545

    history leave days for user to see their leave for certain period is added

--- src/constants/LeaveTypes.js
+++ src/constants/LeaveTypes.js
@@ -3,3 +3,9 @@ export const immediateApprovalLeaveTypes = [
   'Paternity',
   'Paid Time Off',
 ]
+
+export const leaveHistoryDays = [
+  {id: 1, value: 30},
+  {id: 2, value: 60},
+  {id: 3, value: 180},
+]
