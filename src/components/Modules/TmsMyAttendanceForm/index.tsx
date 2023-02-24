04e32754f24e4421f6ad4810d462b4d6fa96fde1
ashok22ganika <ganika.ashok@webexpertsnepal.com>
2023-02-24 15:35:44 +0545

    late arrival issue fix

--- src/components/Modules/TmsMyAttendanceForm/index.tsx
+++ src/components/Modules/TmsMyAttendanceForm/index.tsx
@@ -117,8 +117,7 @@ function TmsMyAttendanceForm({
         punchInIp: IP?.data?.IPv4,
         attendanceDate: moment.utc().startOf('day').format(),
       }
-
-      if (leavesData?.data?.data?.data?.[0]?.halfday !== 'first-half') {
+      if (leavesData?.data?.data?.data?.[0]?.halfDay !== 'first-half') {
         if (!latestAttendance || latestAttendance.length === 0) {
           attendanceParams = {...attendanceParams, isLateArrival: true}
         }
