6c65057c19d625e046f413955527b98f1d8601fb
UtsabJs <neupane.utsav@webexpertsnepal.com>
2023-03-01 16:18:53 +0545

    small bug fixed

--- src/pages/Leave/Apply.js
+++ src/pages/Leave/Apply.js
@@ -380,10 +380,9 @@ function Apply({user}) {
       let LeaveDaysUTC = []
 
       // calculation for maternity, paternity, pto leaves
+      const appliedDate = values?.leaveDatesPeriod?.startOf('day')?._d
       if (leaveType?.isSpecial) {
-        const appliedDate = values?.leaveDatesPeriod?.startOf('day')?._d
         const newDate = new Date(values?.leaveDatesPeriod?._d)
-
         const ArrayofDates = []
         for (let i = 1; i < leaveType?.leaveDays; i++) {
           ArrayofDates.push(
