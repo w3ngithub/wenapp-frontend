255c647554d29ea60e5eba07aade35b6f03c526b
dangolsusan <dangol.susan@webexpertsnepal.com>
2023-04-04 15:55:51 +0545

    path validation done with all the present path in wenapp live

--- src/components/Modules/ProjectModal/index.js
+++ src/components/Modules/ProjectModal/index.js
@@ -415,15 +415,13 @@ function ProjectModal({
                   {
                     required: true,
                     validator: async (rule, value) => {
+                      const regex = /^[A-Z]:\\.*(?<!\\)$/
                       try {
                         if (!value) {
                           throw new Error('Path is required.')
                         }
-                        if (value.trim().length === 0) {
+                        if (value.trim().length === 0 || !regex.test(value)) {
                           throw new Error('Please enter a valid Path.')
-                        }
-                        if (!regex.test(value)) {
-                          throw new Error('Please enter match regex')
                         }
                       } catch (err) {
                         scrollForm(form, 'path')
