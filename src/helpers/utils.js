837b9837fab09b45a20e4eec31a57e08e13ba569
RaulShahi1 <shahi.raul@webexpertsnepal.com>
2023-04-05 14:07:04 +0545

    changed data formate for import export co workers csv files

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -154,7 +154,7 @@ export const csvFileToArray = (string) => {
       continue
     }
     //filtering with index as an extra id is present which is not necessary
-    var words = lines[i].split(',')?.filter((item, index) => index !== 4)
+    var words = lines[i].split(',')
     for (var j = 0; j < words.length; j++) {
       obj[headers[j]?.trim()?.toLowerCase().replaceAll('"', '')] = words[
         j
@@ -162,7 +162,6 @@ export const csvFileToArray = (string) => {
     }
     result.push(obj)
   }
-  console.log({lines, headers, result})
   return result
 }
 
