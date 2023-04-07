212e7128883fb5d8344072bc13f704211a8b28f9
dangolsusan <dangol.susan@webexpertsnepal.com>
2023-04-07 13:44:48 +0545

    approved or rejected added in modal title for overtime

--- src/pages/LogTime/OtherLogTime.jsx
+++ src/pages/LogTime/OtherLogTime.jsx
@@ -184,7 +184,6 @@ function OtherLogTime() {
   }
 
   const handleOpenEditModal = (log) => {
-    setIsAdminTimeLog(true)
     const originalTimelog = logTimeDetails?.data?.data?.data.find(
       (project) => project.id === log.id
     )
@@ -212,11 +211,7 @@ function OtherLogTime() {
       hours: +newLogtime.hours,
       logDate: moment.utc(newLogtime.logDate).format(),
       minutes: +newLogtime.minutes,
-      otStatus: isAdminTimeLog
-        ? newLogtime?.isOt
-          ? 'A'
-          : undefined
-        : newLogtime?.otStatus || (newLogtime?.isOt ? 'P' : undefined),
+      otStatus: newLogtime?.isOt ? 'A' : undefined,
     }
     if (isEditMode)
       UpdateLogTimeMutation.mutate({
